ed47c496d57e1b8ef6ffaea35f93e2229c7c996f
Jeferson Moreira dos Anjos <jefersonanjos@packdocs.com>
2018-09-03 19:27:12 -0300

    add infra interfaces

---
+++ IInfra.Abstract/ISalesRepository.cs
@@ -0,0 +1,10 @@
+﻿using IDomain.Abstract.Entitys;
+using System.Threading.Tasks;
+
+namespace IInfra.Abstract
+{
+	public interface ISalesRepository
+	{
+		Task<string> SaveAsync(ISales sales);
+	}
+}
